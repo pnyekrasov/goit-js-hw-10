6037943eaa2944a4c1924670bb0e8ab5c2fe5d42
pnyekrasov <126099923+pnyekrasov@users.noreply.github.com>
2023-07-01 21:51:16 +0000

    Deploying to gh-pages from @ pnyekrasov/goit-js-hw-10@b7d331a269623828c5d91dc2cd738e5ab2233207 🚀

---
+++ index.1a12c44b.js
@@ -0,0 +1,2 @@
+document.querySelector("breed - select"),live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C;
+//# sourceMappingURL=index.1a12c44b.js.map
---
+++ index.ad999d24.js
@@ -0,0 +1,2 @@
+document.querySelector("breed - select"),live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C;
+//# sourceMappingURL=index.ad999d24.js.map
