4cf6052c48f5dc902a43be4cc3c281aae1cdd9e0
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-04 14:49:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.headers.common['x-api-key'] = API_KEY;
+const API_KEY =
+  'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 // const listCatBreeds = document.querySelector('breed - select');
-// const API_KEY =
-//   'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
 
 // function fetchBreeds() {
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-const API_KEY =
-  'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
-fetch(`https://api.thecatapi.com/v1/breeds123?api_key=S{API_KEY}`)
+// const API_KEY =
+//   'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
+axios
+  .get(`${BASE_URL}`)
   .then(response => {
     console.log(response);
-    return response.json();
-  })
-  .then(data => {
-    console.log('Success', data);
+    // if (!response.ok) {
+    //   console.log('I');
+    // throw new Error(response.status);
+    // }
+    // errorEl.classList.add("is-hidden");
+    console.log(response.data);
+    return response.data;
   })
   .catch(err => {
     console.log('Failure', err);
   });
+
+// loader.classList.add("is-hidden");
+// Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+// })
+// };
+
+// fetch(`${BASE_URL}?api_key=${API_KEY}`)
+//   .then(response => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('Success', data);
+//   })
+// .catch(err => {
+//   console.log('Failure', err);
+// });
 
 // }
 // fetchBreeds();
