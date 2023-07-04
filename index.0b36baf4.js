50f273cb9426e392d025f23fee469d8eb1bb33ee
pnyekrasov <126099923+pnyekrasov@users.noreply.github.com>
2023-07-04 11:50:20 +0000

    Deploying to gh-pages from @ pnyekrasov/goit-js-hw-10@4cf6052c48f5dc902a43be4cc3c281aae1cdd9e0 🚀

--- index.0b36baf4.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds123?api_key=S{API_KEY}").then((function(o){return console.log(o),o.json()})).then((function(o){console.log("Success",o)})).catch((function(o){console.log("Failure",o)}));
-//# sourceMappingURL=index.0b36baf4.js.map
--- index.e8ba855d.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds123?api_key=S{API_KEY}").then((e=>(console.log(e),e.json()))).then((e=>{console.log("Success",e)})).catch((e=>{console.log("Failure",e)}));
-//# sourceMappingURL=index.e8ba855d.js.map
