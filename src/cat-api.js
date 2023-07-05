0309225ebca8268295d25e7793d3be4afc2838d4
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-05 22:20:50 +0300

    HW 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
 
 const API_KEY =
   'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
@@ -9,30 +8,11 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
-  return axios
-    .get(`${BASE_URL}/breeds`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(err =>
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      )
-    );
+  return axios.get(`${BASE_URL}/breeds`);
 }
 
 function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(response => {
-      console.log(response.data[0].breeds[0]);
-      return response.data;
-    })
-    .catch(err =>
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      )
-    );
+  return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`);
 }
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,55 @@
+import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const selectCatBreedsEl = document.querySelector('breed - select');
+const selectCatBreedsEl = document.querySelector('.breed-select');
+const waitingTextEl = document.querySelector('.loader');
+const catInforEl = document.querySelector('.cat-info');
 
-fetchBreeds();
+selectCatBreedsEl.hidden = true;
 
-// function fetchCatByBreed(breedId)
+fetchBreeds()
+  .then(response => {
+    selectCatBreedsEl.innerHTML = createListCatBreeds(response.data);
+    selectCatBreedsEl.hidden = false;
+  })
+  .catch(
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    )
+  )
+  .finally((waitingTextEl.hidden = true));
+
+function createListCatBreeds(array) {
+  return array
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+}
+
+selectCatBreedsEl.addEventListener('change', event => {
+  waitingTextEl.hidden = false;
+  const choiceBreed = event.target.value;
+
+  fetchCatByBreed(choiceBreed)
+    .then(response => {
+      catInforEl.innerHTML = createCartCatInfor(response.data[0]);
+    })
+    .catch(err =>
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
+    )
+    .finally((waitingTextEl.hidden = true));
+});
+
+function createCartCatInfor({
+  url,
+  breeds: {
+    [0]: { name, description, temperament },
+  },
+}) {
+  return `<img src="${url}" alt="${name}">
+<h2>${name}</h2>
+<p>${description}</p>
+<h3>Temperament: </h3>
+<p>${temperament}</p>`;
+}
