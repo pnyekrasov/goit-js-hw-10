2b64ad50f0238284f30971a709fd65e51517f338
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-05 09:28:39 +0300

    src

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const API_KEY =
   'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
 
-axios.defaults.headers.common['x-api-key'] = API_KEY;
-
 const BASE_URL = 'https://api.thecatapi.com/v1';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
   return axios
@@ -14,8 +15,24 @@ function fetchBreeds() {
       return response.data;
     })
     .catch(err =>
-      console.log('Oops! Something went wrong! Try reloading the page!')
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
     );
 }
 
-export { fetchBreeds };
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      console.log(response.data[0].breeds[0]);
+      return response.data;
+    })
+    .catch(err =>
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
+    );
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-import axios from 'axios';
-// import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const API_KEY =
-  'live_0FMlwtQWqd1vJOoTCMUsZdXlnXzXwE1UK0p53tRYcjOaRrrVwcyCZ36uTOxcxR9C';
+const selectCatBreedsEl = document.querySelector('breed - select');
 
-axios.defaults.headers.common['x-api-key'] = API_KEY;
+fetchBreeds();
 
-// const listCatBreeds = document.querySelector('breed - select');
-
-// function fetchCatByBreed(breedId) {
-// fetchBreeds();
-const breedId = 'abys';
-const BASE_URL = 'https://api.thecatapi.com/v1';
-
-axios
-  .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-  .then(response => {
-    console.log(response.data[0].breeds[0]);
-    return response.data;
-  })
-  .catch(err =>
-    console.log('Oops! Something went wrong! Try reloading the page!')
-  );
-// }
+// function fetchCatByBreed(breedId)
